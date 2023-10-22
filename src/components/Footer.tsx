6bfd899bb96b45cd88acb08a02bc4c665ff14f67
Lemon-29 <afislam29@gmail.com>
2023-10-22 22:05:43 +0600

    props

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -34,7 +34,7 @@ const Footer = () => {
           {
             services.map((item) => (
               
-          <li className="hover:text-black cursor-pointer duration-300 relative group overflow-hidden">{item.title}
+          <li key={item.title} className="hover:text-black cursor-pointer duration-300 relative group overflow-hidden">{item.title}
           <span  className="h-[1px] w-full bg-black inline-flex absolute left-0 bottom-0 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"/>
           </li>
           
